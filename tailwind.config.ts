14b7670779d0fd35d53679ba0d6a14a941de7fcb
felipetodev <fe.ossandon.u@gmail.com>
2023-11-20 19:58:31 -0300

    init project ✨

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,20 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        protop: {
+          'light-blue': '#0066FF',
+          space: '#16172E',
+          lavander: '#C9D2E4',
+          seasalt: '#F7F7F7'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
