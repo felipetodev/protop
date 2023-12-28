0c8ee9f5f455f5ab7b2c88290cabed966f309bd4
felipetodev <fe.ossandon.u@gmail.com>
2023-12-27 22:35:02 -0300

    fix: mobile contact anchor

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -14,24 +14,32 @@ type Props = {
 function Contact ({ inbox, phone }: Props) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 bg-protop-seasalt text-xs md:text-lg">
-      <div className="flex flex-col md:flex-row gap-y-[5px] md:gap-y-0 gap-x-[30px] justify-center items-center px-[20px] py-[60px] md:py-[100px] aspect-square sm:aspect-auto">
+      <a
+        rel='noreferrer noopener'
+        href={`mailto:${inbox.mail}`}
+        className="group flex flex-wrap flex-col md:flex-row gap-y-[5px] md:gap-y-0 gap-x-[30px] justify-center items-center px-[20px] py-[60px] md:py-[100px] aspect-square sm:aspect-auto"
+      >
         <span className="text-protop-light-blue">
           {inbox.title}
         </span>
         <InboxIcon className="w-5 h-5 sm:h-auto sm:w-auto text-protop-light-blue" />
-        <a href={`mailto:${inbox.mail}`} className='hidden md:flex text-protop-space hover:underline'>
+        <span className='hidden md:flex text-protop-space group-hover:underline'>
           {inbox.mail}
-        </a>
-      </div>
-      <div className="flex flex-col md:flex-row gap-y-[5px] md:gap-y-0 gap-x-[30px] justify-center items-center bg-protop-light-blue px-[20px] py-[60px] md:py-[100px] aspect-square sm:aspect-auto">
+        </span>
+      </a>
+      <a
+        rel='noreferrer noopener'
+        href={`tel:${phone.phone}`}
+        className="group flex flex-col md:flex-row gap-y-[5px] md:gap-y-0 gap-x-[30px] justify-center items-center bg-protop-light-blue px-[20px] py-[60px] md:py-[100px] aspect-square sm:aspect-auto"
+      >
         <span>
           {phone.title}
         </span>
         <PhoneIcon className="w-5 h-5 sm:h-auto sm:w-auto text-protop-seasalt" />
-        <a href={`tel:${phone.phone}`} className='hidden md:flex text-protop-space hover:underline'>
+        <span className='hidden md:flex text-protop-space group-hover:underline'>
           {phone.phone}
-        </a>
-      </div>
+        </span>
+      </a>
     </div>
   )
 }
